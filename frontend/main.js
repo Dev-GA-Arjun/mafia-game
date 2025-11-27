7cbf0684c55bc8d35fbb9d2746a6dde1cb84da5a
Dev-GA-Arjun <g.a.arjun00@gmail.com>
2025-11-27 13:19:49 +0530

    Initial commit

---
+++ frontend/main.js
@@ -0,0 +1,12 @@
+let gamepin = document.getElementById("pininput");
+let hostButton  = document.getElementById("hostbutton");
+let enterButton = document.getElementById("enterbutton");
+
+async function createRoom(){
+    
+}
+
+
+hostButton.addEventListener("click",createRoom);
+
+enterButton.addEventListener("click",joinRoom);
